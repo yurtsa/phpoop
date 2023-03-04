e1bddbbd4186589ff562cd81bf9a5956b413416c
Юрий <splate@inbox.ru>
2023-03-04 22:37:34 +0300

    '123'

---
+++ users.php
@@ -0,0 +1,28 @@
+<?php
+
+class User{
+    public $name;
+    public $login;
+    public $password;
+
+    public function __construct($name, $login, $password){
+        $this->name=$name;
+        $this->login=$login;
+        $this->password=$password;
+        echo "object {$this->name} created!<hr>";
+    }
+
+    public function __destructor(){
+        echo "object {$this->name}  deleted!<hr>";
+    }
+
+    public function showInfo(){
+        echo $this->name."|".$this->login."|".$this->password."<hr>";
+    }
+}
+
+
+$yurok = new User('Yuriy', 'Splate', 'rfvTGB123');
+$yurok->showInfo();
+unset($yurok);
+
