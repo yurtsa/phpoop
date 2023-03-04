ae59937a4e331d8bf7be848342e3514de3074671
Юрий <splate@inbox.ru>
2023-03-04 20:59:38 +0300

    '123'

--- index.php
+++ index.php
@@ -1,4 +1,65 @@
 <?php
-echo 1;
-echo 2;
-echo 3;
+
+class Tag
+{
+    public $tagName;
+    public $params = [];
+    public $subStringParams;
+    public $stringParams;
+
+    public function __construct($tagName)
+    {
+        $this->tagName = $tagName;
+
+    }
+
+    public function attr($paramOne = false, $paramTwo = false)
+    {
+        if ($paramOne && $paramTwo) {
+            $this->params[] = $paramOne . "=";
+            $this->params[] = $paramTwo . " ";
+            foreach ($this->params as $param) {
+                $this->subStringParams .= " " . $param . " ";
+            }
+        }
+        return $this->subStringParams;
+    }
+
+    public function viewTag($paramOne = false, $paramTwo = false)
+    {
+        $this->stringParams = "<" . $this->tagName;
+        $this->stringParams .= $this->attr($paramOne, $paramTwo);
+        $this->stringParams .= ">";
+        return $this->stringParams;
+    }
+}
+
+class SingleTag extends Tag
+{
+
+}
+
+class PairTag extends Tag
+{
+
+    public function appendChild()
+    {
+        return $this->stringParams = "<" . $this->tagName.$this->stringParams .= "</".$this->tagName.">";
+    }
+}
+
+
+$img = new SingleTag('img');
+$img->attr('src', './nz');
+$img->attr('alt', 'nz');
+$img->attr('hz', 'zh');
+
+$hr = new SingleTag('hr');
+echo $img->viewTag();
+echo $hr->viewTag();
+
+
+$a= new PairTag('a');
+echo $a->appendChild();
+
+
